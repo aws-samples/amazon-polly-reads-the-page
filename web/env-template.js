586841da9a296b3d8d0bd6f8f761a6bb0453ff99
mhavey <haveym@amazon.com>
2022-08-16 06:36:23 -0400

    Update env-template.js
    Remove hardcoded values. This is just a template, the values are not needed.

--- web/env-template.js
+++ web/env-template.js
@@ -1,5 +1,5 @@
 const env = {
-    "S3_BUCKET": "polly-tts-mhavey",
-    "REGION": "us-east-1",
-    "IDP": "us-east-1:e952ac28-5cd4-465d-845b-5a02d7487372",
+    "S3_BUCKET": "your-bucket-name",
+    "REGION": "your-region",
+    "IDP": "your-idp",
 }
